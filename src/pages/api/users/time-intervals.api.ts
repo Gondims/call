d4f7e9320cacbb97abb296824e705c2a175ee3d3
Gondims <leticiagondim1.0@gmail.com>
2023-03-10 22:48:52 -0400

    Deploy

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { unstable_getServerSession } from 'next-auth'
-import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { buildNextAuthOptions } from '../auth/[...nextAuth].api'
 import { z } from 'zod'
 import { prisma } from '../../../lib/prisma'
 
